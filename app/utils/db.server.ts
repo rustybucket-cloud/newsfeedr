aa6420e75dc0198b1b32db8b74c020c40d5c29b7
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2023-03-06 11:58:55 -0700

    reintroduce prisma

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -4,25 +4,23 @@
 /* eslint-disable vars-on-top */
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
 
-// let prisma: PrismaClient;
+declare global {
+  // eslint-disable-next-line no-var
+  var __db: PrismaClient | undefined;
+}
 
-// declare global {
-//   // eslint-disable-next-line no-var
-//   var __db: PrismaClient | undefined;
-// }
-
-// // this is needed because in development we don't want to restart
-// // the server with every change, but we want to make sure we don't
-// // create a new connection to the DB with every change either.
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!global.__db) {
-//     global.__db = new PrismaClient();
-//   }
-//   prisma = global.__db;
-// }
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient();
+  }
+  prisma = global.__db;
+}
 
 export { prisma };
